0717306d16c6e4768511c662727d750930b4c740
Alvaro do Campo <alvarodocampogonzalez@gmail.com>
2026-06-18 18:37:18 +0200

    Configuracion render

--- confing/settings.py
+++ confing/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-s$22_$ys3cc$@e)f3*$t5c*%a6tmgw)uky+^2ctbc+!+#wmc&&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
